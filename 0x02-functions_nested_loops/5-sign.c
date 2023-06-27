46353692011f285828ed304e307db3b0d75aedec
YoussefGhamaz <yaghamaz@gmail.com>
2023-06-26 19:55:06 -0700

    g

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_sign - print + if n is greater than zero.
+ *            O if n is zero and - if n is less
+ *            than zero.
+ * @n: takes integer type input for function.
+ *
+ * Return: 1 if + , O if O and -1 if -
+*/
+
+int print_sign(int n)
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98.c -takes apointer to an int parameter
+ *                and adaptes the value it points to 98
+ *
+ * @n: int parameter
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
