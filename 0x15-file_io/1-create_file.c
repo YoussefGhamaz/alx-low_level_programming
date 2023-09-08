05283dd05e9b477ccaf30069e573a52502bbb30a
YoussefGhamaz <135194718+YoussefGhamaz@users.noreply.github.com>
2023-09-08 20:28:20 +0300

    gg

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * create_file - Creates a file.
@@ -13,12 +13,12 @@ int create_file(const char *filename, char *text_content)
 	int fd, w, len = 0;
 
 	if (filename == NULL)
-	return (-1);
+		return (-1);
 
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-		len++;
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
